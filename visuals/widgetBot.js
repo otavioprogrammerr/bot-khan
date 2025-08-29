9a82b9ef612abb7cc2da76aab0b25ebead3349a6
otavioprogrammerr <otavioaugustooliveiraruth@gmail.com>
2025-08-29 20:51:28 -0300

    Update widgetBot.js

--- visuals/widgetBot.js
+++ visuals/widgetBot.js
@@ -1,13 +1,28 @@
-if(!device.mobile) {
+// widgetBot.js - Plataform Store
+
+if (!device.mobile) {
     const script = Object.assign(document.createElement('script'), {
         src: 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3',
         async: true,
+        defer: true,
         onload: () => {
-            const discEmbed = new Crate({ server: '1286573512831533056', channel: '1286573601687867433',
-                location: ['bottom', 'right'], notifications: true, indicator: true, allChannelNotifications: true,
-                defer: false, color: '#000000'
+            const discEmbed = new Crate({
+                server: '1286573512831533056',
+                channel: '1286573601687867433',
+                location: ['bottom', 'right'],
+                notifications: true,
+                indicator: true,
+                allChannelNotifications: true,
+                color: '#5865F2' // Discord Blurple for better branding
             });
-            plppdo.on('domChanged', () => window.location.href.includes("khanacademy.org/profile") ? discEmbed.show() : discEmbed.hide() );
+
+            plppdo.on('domChanged', () => {
+                if (window.location.href.includes("khanacademy.org/profile")) {
+                    discEmbed.show();
+                } else {
+                    discEmbed.hide();
+                }
+            });
         }
     });
     document.body.appendChild(script);
